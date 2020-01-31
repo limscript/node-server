d3bcbf37cf1e5bf6b7d06b92b1e004d3d8ca4534
李萌 <lim16@mingyuanyun.com>
2020-01-31 15:41:09 +0800

    feat： express告一段落

---
+++ blog-express/controller/blog.js
@@ -0,0 +1,77 @@
+const xss = require('xss')
+const { exec } = require('../db/mysql')
+
+const getList = (author, keyword) => {
+  let sql = `select * from blogs where 1=1 `
+  if (author) {
+    sql += `and author='${author}' `
+  }
+  if (keyword) {
+    sql += `and title like '%${keyword}%' `
+  }
+  sql += `order by createtime desc;`
+
+  //返回promise
+  return exec(sql)
+}
+
+const getDetail = (id) => {
+  const sql = `select * from blogs where id=${id} `
+
+  //返回promise
+  return exec(sql).then((rows) => {
+    return rows[0]
+  })
+}
+
+const newBlog = (blogData = {}) => {
+  //blogData是一个博客对象，包含 title content 属性
+  const title = xss(blogData.title)
+  const content = xss(blogData.content)
+  const author = blogData.author
+  const createTime = Date.now()
+
+  const sql = `
+  insert into blogs (title, content, createtime, author)
+  values ('${title}', '${content}', '${createTime}', '${author}')
+  `
+  //返回promise
+  return exec(sql).then((insertData) => {
+    return {
+      id: insertData.insertId
+    }
+  })
+}
+
+const updateBlog = (id, blogData = {}) => {
+  //id 就是要更新博客的ID
+  //blogData 是一个博客对象，包含 title content 属性
+  const title = xss(blogData.title)
+  const content = xss(blogData.content)
+  console.log(title, content, id);
+
+  const sql = `update blogs set title='${title}', content='${content}' where id=${id}`
+  //返回promise
+  return exec(sql).then((updateData) => {
+    console.log(updateData);
+    if (updateData.affectedRows > 0) {
+      return true
+    }
+    return false
+  })
+}
+
+const delBlog = (id, author) => {
+  //id 就是要删除博客的ID
+  const sql = `delete from blogs where id='${id}' and author='${author}'`
+  //返回promise
+  return exec(sql).then((delData) => {
+    console.log(delData);
+    if (delData.affectedRows > 0) {
+      return true
+    }
+    return false
+  })
+}
+
+module.exports = { getList, getDetail, newBlog, updateBlog, delBlog }
---
+++ blog-express/controller/user.js
@@ -0,0 +1,13 @@
+const { exec } = require('../db/mysql')
+
+const login = (username, password) => {
+
+  const sql = `select username, realname from users where username='${username}' and password='${password}'`
+
+  //返回promise
+  return exec(sql).then((row) => {
+    return row[0] || {}
+  })
+}
+
+module.exports = { login }
